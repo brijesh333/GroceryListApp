2f4fe730d5463bdf4d5a0ce70e13cbce0b74583c
brijesh333 <patelbrijesh333@gmail.com>
2016-11-20 00:50:52 +0530

    added item to list

--- js/app.js
+++ js/app.js
@@ -31,16 +31,33 @@ app.controller('HomeController',['$scope',function ($scope) {
     $scope.appTitle="Grocery list app";
 }]);
 
-app.controller('GroceryListItemController',["$scope","$routeParams",function ($scope ,$routeParams) {
-    $scope.groceryItem=[
-        {completed: true, itemName: 'milk', date: '2014-10-01'},
-        {completed: true, itemName: 'coookies', date: '2014-10-02'},
-        {completed: true, itemName: 'ice cream', date: '2014-10-03'},
-        {completed: true, itemName: 'eggs', date: '2014-10-05'},
-        {completed: true, itemName: 'bread', date: '2014-10-10'},
-        {completed: true, itemName: 'potatoes', date: '2014-10-21'},
-        {completed: true, itemName: 'rice', date: '2014-10-25'},
+app.service("GroceryService",function () {
+    var groceryService={};
+    groceryService.groceryItem=[
+        {id:1,completed: true, itemName: 'milk', date: '2014-10-01'},
+        {id:2,completed: true, itemName: 'coookies', date: '2014-10-02'},
+        {id:3,completed: true, itemName: 'ice cream', date: '2014-10-03'},
+        {id:4,completed: true, itemName: 'eggs', date: '2014-10-05'},
+        {id:5,completed: true, itemName: 'bread', date: '2014-10-10'},
+        {id:6,completed: true, itemName: 'potatoes', date: '2014-10-21'},
+        {id:7,completed: true, itemName: 'rice', date: '2014-10-25'},
     ];
 
-    $scope.rp="Route parameter value is "+ $routeParams.id +$routeParams.newParam;
+    groceryService.save=function(entry){
+        groceryService.groceryItem.push(entry);
+    }
+    return groceryService;
+});
+
+app.controller('GroceryListItemController',["$scope","$routeParams","$location","GroceryService",function ($scope ,$routeParams,$location,GroceryService) {
+
+    //console.log($GroceryService.groceryItem[0].itemName);
+    $scope.groceryItem=GroceryService.groceryItem;
+    //$scope.rp="Route parameter value is "+ $routeParams.id +$routeParams.newParam;
+    $scope.groceryItems={ id:8,completed: true, itemName:"cheese", date:new Date() };
+    
+    $scope.save=function () {
+        GroceryService.save($scope.groceryItems);
+        $location.path("/");
+    }
 }]);
